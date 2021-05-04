346d17b09cddd22f9e396b532cce36053380304b
Maxel <gigaabait@yandex.ru>
2021-05-04 17:43:29 +0400

    port

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ require("./app/routes/achievement.routes.js")(app);
 require("./app/routes/olympiad.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
